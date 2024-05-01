2ef524ecfbf9563a4549b8e3fd5c350cdc27d11e
Luke Brider-Woolard <luke.brider-woolard@cgi.com>
2024-05-01 12:44:40 +0100

    kinda works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CdkDropList, CdkDragDrop,  moveItemInArray } from '@angular/cdk/drag-drop'
+import { CdkDrag, CdkDropList, CdkDropListGroup, CdkDragDrop, CdkDragMove, moveItemInArray } from '@angular/cdk/drag-drop'
+import { ViewportRuler } from "@angular/cdk/overlay";
 import { HeaderComponent } from './components/header/header.component';
 import { ItemBoxComponent } from './components/item-box/item-box.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CdkDropList, HeaderComponent, ItemBoxComponent,],
+  imports: [RouterOutlet, CdkDrag, CdkDropList, CdkDropListGroup, HeaderComponent, ItemBoxComponent, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  @ViewChild(CdkDropListGroup) listGroup: CdkDropListGroup<CdkDropList>;
+  @ViewChild(CdkDropList) placeholder: CdkDropList;
+
+  public gridSize = 50;
+  public gridMargin = 10;
   public items = [
     'textbox',
     'image'
   ]
 
   drop(event: CdkDragDrop<string[]>) {
+    console.log(event);
     moveItemInArray(this.items, event.previousIndex, event.currentIndex);
   }
 
--- src/app/components/item-box/item-box.component.ts
+++ src/app/components/item-box/item-box.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { NgIf } from '@angular/common';
-import { CdkDrag, CdkDragHandle, DragRef, Point } from '@angular/cdk/drag-drop';
+import { CdkDrag, CdkDragMove, CdkDragHandle } from '@angular/cdk/drag-drop';
 import { MatIconModule } from '@angular/material/icon';
 import { TextBoxComponent } from '../text-box/text-box.component';
 import { ImageBoxComponent } from '../image-box/image-box.component';
@@ -14,14 +14,9 @@ import { ImageBoxComponent } from '../image-box/image-box.component';
 })
 export class ItemBoxComponent {
   @Input() type: string;
+  // @Output() dragMovedEvent = new EventEmitter<CdkDragMove>();
 
-  public gridSize = 50;
-  public gridMargin = 10;
-
-  // computeDragRenderPos(pos: Point, dragRef: DragRef) {
-  //   return {
-  //     x: Math.floor(pos.x / 100) * 100,
-  //     y: Math.floor(pos.y / 100) * 100
-  //   };
+  // dragMoved(event: CdkDragMove) {
+  //   this.dragMovedEvent.emit(event);
   // }
 }
