074bbf18fb0d71579fdb882a0c0cad913a0aa41f
Luke Brider-Woolard <luke.brider-woolard@cgi.com>
2024-05-02 00:18:24 +0100

    almost working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,75 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CdkDrag, CdkDropList, CdkDropListGroup, CdkDragDrop, CdkDragMove, moveItemInArray } from '@angular/cdk/drag-drop'
+import { AngularDraggableModule } from 'angular2-draggable';
+import { IResizeEvent } from 'angular2-draggable/lib/models/resize-event'
+import { MatIconModule } from '@angular/material/icon';
 import { HeaderComponent } from './components/header/header.component';
-import { ItemBoxComponent } from './components/item-box/item-box.component';
-import { NgFor } from '@angular/common';
+import { TextBoxComponent } from './components/text-box/text-box.component';
+import { ImageBoxComponent } from './components/image-box/image-box.component';
+import { NgFor, NgIf } from '@angular/common';
+
+export class Item {
+  id: number;
+  type: string;
+  offsetX: number;
+  offsetY: number;
+  width: number;
+  height: number;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CdkDrag, CdkDropList, CdkDropListGroup, HeaderComponent, ItemBoxComponent, NgFor],
+  imports: [RouterOutlet, AngularDraggableModule, MatIconModule, HeaderComponent, TextBoxComponent, ImageBoxComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  public gridSize = 50;
-  public gridMargin = 10;
-  public items = [
-    { id: 1, type: 'textbox' },
-    { id: 2, type: 'image' }
+  public gridSize = 100;
+
+  public items: Item[] = [
+    { id: 1, type: 'textbox', offsetX: 0, offsetY: 0, width: 200, height: 200 },
+    { id: 2, type: 'image', offsetX: 0, offsetY: 200, width: 200, height: 200 }
   ]
 
-  drop(event: CdkDragDrop<{id: number, type: string}[]>) {
-    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+  public offset = { x: 0, y: 0 };
+  public size = { "width": 0, "height": 0 };
+
+  onMoveEnd(event: { x: number, y: number }, updatedItem: Item) {
+    this.items.map((item) => {
+      if (item.id === updatedItem.id) {
+        return {
+          ...item,
+          offsetX: event.x,
+          offsetY: event.y
+        };
+      }
+      return item;
+    });
+  }
+
+  onResizeStop(event: IResizeEvent, updatedItem: Item) {
+    this.items.map((item) => {
+      if (item.id === updatedItem.id) {
+        return {
+          ...item,
+          width: event.size.width,
+          height: event.size.height
+        };
+      }
+      return item;
+    });
   }
 
   add(itemType: string) {
     const highestId = this.items.reduce(function(prev, current) {
       return (prev && prev.id > current.id) ? prev : current
     });
-    const newItem = { id: highestId.id++, type: itemType };
+    const newItem = { id: highestId.id++, type: itemType, offsetX: 0, offsetY: 0, width: 200, height: 200 };
     this.items.push(newItem)
   }
 
   save() {
-    console.log("saving...")
+    console.log(this.items)
   }
 }
