874a0766f74e14a7e6569cb467a3ba7a36760e7d
Luke Brider-Woolard <luke.brider-woolard@cgi.com>
2024-05-08 13:37:59 +0100

    added delete functionality

--- src/app/components/item-selection-bar/item-selection-bar.component.ts
+++ src/app/components/item-selection-bar/item-selection-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { ItemBoxComponent } from '../item-box/item-box.component';
 
 @Component({
   selector: 'app-item-selection-bar',
   standalone: true,
-  imports: [ItemBoxComponent],
+  imports: [MatIconModule, ItemBoxComponent],
   templateUrl: './item-selection-bar.component.html',
   styleUrl: './item-selection-bar.component.css'
 })
